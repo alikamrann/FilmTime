926d9cb951983ad370b3d2b65142a3ee8a5d354d
ali kamran <ali.kamran.98.98@gmail.com>
2023-10-12 22:51:09 +0330

    add screen module

--- app/src/main/java/io/filmtime/MainActivity.kt
+++ app/src/main/java/io/filmtime/MainActivity.kt
@@ -2,9 +2,24 @@ package io.filmtime
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import io.filmtime.feature.movie.detail.MovieDetailScreen
+import io.filmtime.feature.movie.detail.MovieDetailViewModel
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    val viewModel by viewModels<MovieDetailViewModel>()
+    setContent {
+      Surface(modifier = Modifier.fillMaxWidth(), color = MaterialTheme.colorScheme.background) {
+
+        MovieDetailScreen(viewModel = viewModel)
+      }
+    }
   }
 }
--- feature/movie-detail/build.gradle.kts
+++ feature/movie-detail/build.gradle.kts
@@ -31,16 +31,18 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
 dependencies {
 
+    implementation(project(":domain:tmdb-movies"))
+    implementation(project(":data:model"))
     implementation(libs.core.ktx)
     implementation(libs.appcompat)
     implementation(libs.material)
@@ -56,4 +58,7 @@ dependencies {
 
     implementation(libs.activity.compose)
     implementation(platform(libs.compose.bom))
+
+    implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.lifecycle.viewmodel.compose.runtime)
 }
